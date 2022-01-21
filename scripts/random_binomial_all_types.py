fixed minor mistake for sigmoid units

scripts/random_binomial_all_types.py
@@ -35,7 +35,7 @@ import os
 import argparse
 
 from mar.causality.dags import DirectedAcyclicGraph
-from mar.causality.scm import BinomialBinarySCM
+from mar.causality.scm import BinomialBinarySCM, SigmoidBinarySCM
 from mar.recourse import recourse_population, save_recourse_result
 
 
@@ -68,15 +68,19 @@ def generate_problem(N, p, min_in_degree, out_degree, max_uncertainty, seed=42):
     return scm_return, y_name
 
 
-def load_problem(path):
-    scm = BinomialBinarySCM.load(path)
+def load_problem(path, type='binomial'):
+    scm = None
+    if type == 'binomial':
+        scm = BinomialBinarySCM.load(path)
+    elif type == 'sigmoid':
+        scm = SigmoidBinarySCM.load(path)
     y_name = scm.predict_target
     return scm, y_name
 
 
 def run_experiment(N_nodes, p, max_uncertainty, min_in_degree, out_degree, seed, N,
                    lbd, gamma, thresh, savepath, use_scm_pred=False, iterations=5, t_types='both',
-                   scm_loadpath=None):
+                   scm_loadpath=None, scm_type=None):
     try:
         os.mkdir(savepath)
     except OSError as err:
@@ -94,7 +98,7 @@ def run_experiment(N_nodes, p, max_uncertainty, min_in_degree, out_degree, seed,
         scm, y_name = generate_problem(N_nodes, p, min_in_degree, out_degree, max_uncertainty, seed=seed)
         costs = np.random.uniform(0, lbd / (N_nodes - 1), N_nodes - 1)
     else:
-        scm, y_name = load_problem(scm_loadpath)
+        scm, y_name = load_problem(scm_loadpath, type=scm_type)
         costs = np.load(scm_loadpath + 'costs.npy')
 
 
@@ -212,9 +216,9 @@ if __name__ == '__main__':
     parser.add_argument("--t_type", help="target types, either one of improvement and acceptance or both",
                         default="both", type=str)
     parser.add_argument("--scm_loadpath", help="loadpath for scm to be used", default=None, type=str)
+    parser.add_argument("--scm_type", help="type of scm, either binomial or sigmoid", default='binomial', type=str)
 
     parser.add_argument("--logging_level", help="logging-level", default=20, type=int)
-
 
     args = parser.parse_args()
 
@@ -239,4 +243,4 @@ if __name__ == '__main__':
     run_experiment(args.N_nodes, args.p, args.max_uncertainty, args.min_in_degree, args.out_degree,
                    args.seed, args.N, args.lbd, args.gamma, args.thresh, savepath_config,
                    iterations=args.n_iterations, use_scm_pred=False, t_types=args.t_type,
-                   scm_loadpath=args.scm_loadpath)
+                   scm_loadpath=args.scm_loadpath, scm_type=args.scm_type)

src/mar/causality/evaluate_sigmoidal.py
@@ -7,10 +7,6 @@ savepath = 'scms/'
 
 # ex 5
 
-# DEFINE DATA GENERATING MECHANISM
-
-scm_dir = 'example5/'
-
 # try:
 #     os.mkdir(savepath + scm_dir)
 # except FileExistsError as err:
@@ -44,7 +40,7 @@ costs = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5])
 y_name = 'y'
 scm.set_prediction_target(y_name)
 
-scm.sample_context(100)
+context = scm.sample_context(100)
 # scm.compute_node('y')
 data = scm.compute()
 
@@ -52,10 +48,23 @@ obs = data.iloc[0, :]
 obs = obs.loc[obs.index != 'y']
 
 scm_abd = scm.abduct(obs)
-smpl = scm_abd.sample_context(100)
+smpl = scm_abd.sample_context(1000)
 smpl_data = scm_abd.compute()
 
+# hand-engineer the intervention
 
+scm_abd.compute(do={'x1': 1.0}).describe()['y']
+
+scm_true = scm.copy()
+scm_true.set_noise_values(smpl.iloc[0, :].to_dict())
+scm_true.compute(do={'x1': 1.0}).describe()['y']
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(smpl['u_y'])
+plt.show()
 
 from mar.backend.dist import TransformedUniform
 

src/mar/causality/scm.py
@@ -694,7 +694,7 @@ class SigmoidBinarySCM(BinomialBinarySCM):
                         linear_comb += self.model[par]['values'] - 0.5
                     else:
                         linear_comb += torch.tensor(obs[par]) - 0.5
-            return linear_comb * 4
+            return linear_comb * 2
         else:
             return super()._linear_comb_parents(node, obs=obs)
 