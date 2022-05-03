a627b1d9287af3f975b438e56a47565e746f83a0
gcskoenig <gunnar.koenig@pm.me>
2022-05-03 22:25:28 +0200

    final attempt

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -4,7 +4,7 @@ import random
 import os
 
 # savepath = '../experiments/remote-experiments/test_generic/greenfield/'
-savepath = '~/data/mcr-experiments/test_generic/3var-noncausal/'
+savepath = '/home/gcsk/data/mcr-experiments/test_generic/3var-noncausal/'
 
 id = random.randint(0, 2**10)
 print(id)
@@ -16,7 +16,7 @@ run_experiment('3var-noncausal', 2000, 0.9, 0.5, 10, savepath + f'_{id}',
 compile_experiments(savepath, assess_robustness=False, scm_name='3var-noncausal')
 
 
-savepath = '~/data/experiments/mcr-experiments/test_generic/3var-causal/'
+savepath = '/home/gcsk/data/experiments/mcr-experiments/test_generic/3var-causal/'
 
 id = random.randint(0, 2**10)
 print(id)
